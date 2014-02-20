39f3f036b8d662f61b1553384401b09cc01c87a4
Stephen Babineau <tien_ralter@hotmail.com>
2014-02-20 09:04:09 -0800

    Add Errorclass and map_uri tests

--- http_server.py
+++ http_server.py
@@ -10,12 +10,13 @@ def start_server():
     server_socket = socket.socket(socket.AF_INET,
                                   socket.SOCK_STREAM,
                                   socket.IPPROTO_IP)
-    server_socket.bind(("127.0.0.1", 50000))
+    server_socket.bind(("127.0.0.1", 80))
     server_socket.listen(1)
 
     while True:  # endless loop to permit server to continue echo function
         conn, addr = server_socket.accept()
         message = recv_data(conn)
+        msg_split = split_header(message)
         conn.close()
 
 
@@ -23,15 +24,14 @@ def recv_data(connection):
     #capping loops for receiving recv_data
     msg = ''
     while True:
-        buff = connection.recv(32)
-        message += buff
+        buff = connection.recv(4096)
+        msg += buff
         if not buff:
             return msg
 
 
-def parse_header(data):
-    #take message, split, then parsemethod and determine whether okay or
-    #raise Exception
+def split_header(data):
+    #splits data and returns method, URI, protocol
     splitdata = data.split('\r\n', 1)
     splitheader = splitdata[0].split(' ', 3)
     return splitheader
@@ -45,3 +45,12 @@ def map_uri(uri):
 def build_response():
     #builds response/errors
     pass
+
+
+class NotFoundError(Exception):
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
--- http_tests.py
+++ http_tests.py
@@ -1,47 +1,54 @@
 import http_server
 import unittest
+import os
 from email.utils import formatdate
 
 
-class StartServerTests(unittest.TestCase):
-
-    def test1(self):
-        pass
-
-
-class RecvDataTests(unittest.TestCase):
-
-    def test1(self):
-        pass
+# class StartServerTests(unittest.TestCase):
+#     #assumed working at moment
+#     def test1(self):
+#         pass
 
 
-class ParseHeaderTests(unittest.TestCase):
+# class RecvDataTests(unittest.TestCase):
+
+#     def test1(self):
+#         #no idea how to test this function
+#         pass
+
+
+class SplitHeaderTests(unittest.TestCase):
 
     def setUp(self):
         self.recv = "GET /path/to/file/index.html HTTP/1.1 \r\n StuffThings"
-        self.header = "GET /path/to/file/index.html HTTP/1.1"
         self.method = "GET"
         self.URI = "/path/to/file/index.html"
         self.protocol = "HTTP/1.1"
-        self.join = ''
-
-    # def test_split(self):
-    #     self.assertEqual(self.header, self.join.join(http_server.parse_header
-    #                      (self.recv)), "Parse header from correct request")
 
     def test_split_method(self):
-        self.assertEqual(self.method, http_server.parse_header(self.recv)[0])
+        self.assertEqual(self.method, http_server.split_header(self.recv)[0])
 
     def test_split_URI(self):
-        self.assertEqual(self.URI, http_server.parse_header(self.recv)[1])
+        self.assertEqual(self.URI, http_server.split_header(self.recv)[1])
 
     def test_split_protocol(self):
-        self.assertEqual(self.protocol, http_server.parse_header(self.recv)[2])
+        self.assertEqual(self.protocol, http_server.split_header(self.recv)[2])
 
 
 class MapUriTests(unittest.TestCase):
 
-    def test1(self):
+    def setUp(self):
+        self.imguri = "/webroot/images/JPEG_example.jpg"
+        self.diruri = "/webroot/images"
+
+    def test_file_exist(self):
+        self.assertEqual(http_server.map_uri("/images/JPEG_example.jpg"),
+                         self.imguri)
+
+    def test_dir_exist(self):
+        self.assertEqual(http_server.map_uri("/images"), self.diruri)
+
+    def tearDown(self):
         pass
 
 
